b3f44e23a7c22a5d9438ce8825315988401bf9c3
Daniel <danielk872@gmail.com>
2021-11-08 11:57:49 -0300

    login quse funcionando

--- js/incluir_pessoa.js
+++ js/incluir_pessoa.js
@@ -2,36 +2,45 @@ $(function(){
 
     $ (document).on("click", "#btIncluirPessoa", function() {
         nome = $("#campoNome").val();
+        senha = $("#campoSenha").val();
+        con_senha = $("#campoCon_Senha").val();
         email = $("#campoEmail").val();
-        cpf = $("campoCpf").val();
-        idade = $("campoIdade").val();
+        cpf = $("#campoCpf").val();
+        idade = $("#campoIdade").val();
 
-        var dados = JSON.stringify({nome: nome, cpf: cpf, email: email, idade: idade});
+        if (senha != con_senha) {
+            alert("ERRO: Erro ao confirmar a senha");
+        } else {
 
-        $.ajax({
-            url: 'https://localhost:5000/incluir_pessoa',
-            type: 'POST',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: dados,
-            success: pessoaInluida,
-            error: erroAoIncluir
-        });
+            var dados = JSON.stringify({nome: nome, cpf: cpf, email: email, idade: idade});
 
-        function pessoaInluida (retorno) {
-            if (retorno.resultado == "ok") {
-                alert("Pessoa incluída com sucesso!");      
-                $("campoNome").val("");
-                $("campoEmail").val("");
-                $("campoCpf").val("");
-                $("campoIdade").val("");
-            } else {
-                alert(retorno.resultado + ":" + retorno.detalhes)
+            $.ajax({
+                url: 'https://localhost:5000/incluir_pessoa',
+                type: 'POST',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: dados,
+                success: pessoaInluida,
+                error: erroAoIncluir
+            });
+
+            function pessoaInluida (retorno) {
+                if (retorno.resultado == "ok") {
+                    alert("Pessoa incluída com sucesso!");      
+                    $("campoNome").val("");
+                    $("campoEmail").val("");
+                    $("campoSenha").val("");
+                    $("campoCon_Senha").val("");
+                    $("campoCpf").val("");
+                    $("campoIdade").val("");
+                } else {
+                    alert(retorno.resultado + ":" + retorno.detalhes)
+                }
             }
-        }
 
-        function erroAoIncluir (retorno) {
-            alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+            function erroAoIncluir (retorno) {
+                alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+            }
         }
     });
 });
