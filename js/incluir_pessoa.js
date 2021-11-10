76d35ac3a01c280c0dbd01a79682b4add09cf227
Braian <braianwandalen.ifc@gmail.com>
2021-11-10 17:14:20 -0300

    Commit adicional
    incluir_pessoa.js atualizado

--- js/incluir_pessoa.js
+++ js/incluir_pessoa.js
@@ -1,12 +1,23 @@
 $(function(){
 
     $ (document).on("click", "#btIncluirPessoa", function() {
+        var heranca = 0
+
         nome = $("#campoNome").val();
         senha = $("#campoSenha").val();
         con_senha = $("#campoCon_Senha").val();
         email = $("#campoEmail").val();
         cpf = $("#campoCpf").val();
         idade = $("#campoIdade").val();
+
+        profi = $("input[name='user']:checked").val();
+
+        if (profi == "aluno") {
+            heranca = 2
+        }
+        if (profi == "prof"){
+            heranca = 1
+        }
 
         if (senha != con_senha) {
             alert("ERRO: Erro ao confirmar a senha");
@@ -15,16 +26,16 @@ $(function(){
             var dados = JSON.stringify({nome: nome, cpf: cpf, email: email, idade: idade});
 
             $.ajax({
-                url: 'http://localhost:5000/incluir_pessoa',
+                url: 'http://localhost:5000/incluir_pessoa/'+heranca,
                 type: 'POST',
                 dataType: 'json',
                 contentType: 'application/json',
                 data: dados,
-                success: pessoaInluida,
+                success: pessoaIncluida,
                 error: erroAoIncluir
             });
 
-            function pessoaInluida (retorno) {
+            function pessoaIncluida (retorno) {
                 if (retorno.resultado == "ok") {
                     alert("Pessoa incluída com sucesso!");      
                     $("campoNome").val("");
